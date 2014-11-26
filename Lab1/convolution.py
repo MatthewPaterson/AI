b6f4ea6333a44d029026a5ae90c1fbf48028eb5f
Matthew Paterson <matty.mep@gmail.com>
2014-11-26 16:09:11 +0000

    Horrible code. Works but needs to be improved

--- Lab1/convolution.py
+++ Lab1/convolution.py
@@ -1,5 +1,8 @@
-import numpy,os,math,itertools
+import numpy,os,math,itertools,random
 import matplotlib.pyplot as plt
+from sklearn.cross_validation import KFold
+
+
 
 
 
@@ -121,34 +124,6 @@ def sign(val):
 
 	return x
 
-# def zcr(lines,filename,window):
-# 	sign_change = 0
-# 	last_sign = 1
-# 	counter = 0 
-# 	total = 0
-# 	counter = 0
-# 	lists = []
-# 	while (counter < len(lines)):
-
-# 		window = counter - 80
-# 		start = max(0,window)
-# 		total = lines[(start):(counter)]
-
-# 		#for x in total:
-# 		i = start
-# 		sy = 0
-# 		while start < counter:
-
-# 			y = sign(lines[start]) - sign(lines[start-1])
-
-# 			start += 1
-# 			sy += y
-		
-# 		print (sy)
-# 		lists += [sy]
-# 		counter +=1
-
-# 		plt.plot(lists)
 
 def zcr(lines, window):
 
@@ -160,7 +135,6 @@ def zcr(lines, window):
 
 		low = max(1, x-80)
 
-		# Over window
 		while low < x:
 			val = sign(lines[low]) - sign(lines[low - 1])
 			summation += abs(val)
@@ -209,17 +183,146 @@ def feature_value():
 		file.write(str(zipped_list[i][1]) + '\n')
 		file.write(str(zipped_list[i][2]) + '\n')
 		file.close()
-
-
-
-
 	return
 
+def read():
+	list_energy = []
+	list_magnitude = []
+	list_zcr = []
+	list_type = []
+
+	for root,dirs,files in os.walk("/users/level4/1102374p/AI/Lab1/files/"):
+		for f in files:
+			with open("/users/level4/1102374p/AI/Lab1/files/"+f) as line:
+
+
+				lines = []
+				lines = line.read().splitlines()
+				list_energy.append(lines[0])
+				list_magnitude.append(lines[1])
+				list_zcr.append(lines[2])
+				if f.startswith("silence"):
+					list_type.append(0)
+				else:
+					list_type.append(1)
+
+	return list_energy,list_magnitude,list_zcr,list_type
+
+
+def kfold(energy,magnitude,zcr,types,k):
+	random_sound = []
+	random_silence = []
+
+	e_dev_sound = []
+	m_dev_sound = []
+	z_dev_sound = []
+	t_dev_sound = []
+
+	e_dev_no_sound = []
+	m_dev_no_sound = []
+	z_dev_no_sound = []
+	t_dev_no_sound = []
+
+	e_average_sound = 0
+	m_average_sound = 0 
+	z_average_sound = 0
+
+	e_average_no_sound = 0
+	m_average_no_sound = 0 
+	z_average_no_sound = 0
+
+	zipped = zip(energy,magnitude,zcr,types)
+
+	sound = zipped[0:50]
+	silence = zipped[50:100]
+
+	for i in range(5):
+		temp1 = random.choice(sound)
+		temp2 = random.choice(silence)
+		random_sound.append(temp1)
+		random_silence.append(temp2)
+
+	test_set = random_sound + random_silence
+
+	for i in zipped[:]:
+		if i in test_set:
+			zipped.remove(i)
+
+	sound = zipped[0:45]
+	no_sound = zipped[45:90]
+
+	for i in range(len(sound)):
+		e_average_sound += float(sound[i][0])
+		m_average_sound += float(sound[i][1])
+		z_average_sound += float(sound[i][2])
+
+	e_average_sound = e_average_sound/len(sound)
+	m_average_sound = m_average_sound/len(sound)
+	z_average_sound = z_average_sound/len(sound)
+
+	for i in range(len(no_sound)):
+		e_average_no_sound += float(no_sound[i][0])
+		m_average_no_sound += float(no_sound[i][1])
+		z_average_no_sound += float(no_sound[i][2])
+
+	e_average_no_sound = e_average_no_sound/len(no_sound)
+	m_average_no_sound = m_average_no_sound/len(no_sound)
+	z_average_no_sound = z_average_no_sound/len(no_sound)
 
 
 
+
+
+
+
+
+
+
+
+	for i in range(len(sound)):
+		e_dev_sound.append(((float(sound[i][0]) - e_average_sound))**2)
+		m_dev_sound.append(((float(sound[i][1]) - m_average_sound))**2)
+		z_dev_sound.append(((float(sound[i][2]) - z_average_sound))**2)
+		t_dev_sound.append(sound[i][3])
+
+	for i in range(len(no_sound)):
+		e_dev_no_sound.append(((float(no_sound[i][0]) - e_average_no_sound))**2)
+		m_dev_no_sound.append(((float(no_sound[i][1]) - m_average_no_sound))**2)
+		z_dev_no_sound.append(((float(no_sound[i][2]) - z_average_no_sound))**2)
+		t_dev_no_sound.append(no_sound[i][3])
+
+	e_var_sound = sum(e_dev_sound)/(len(sound)-1)
+	m_var_sound = sum(m_dev_sound)/(len(sound)-1)
+	z_var_sound = sum(z_dev_sound)/(len(sound)-1)
+
+	e_var_no_sound = sum(e_dev_no_sound)/(len(no_sound)-1)
+	m_var_no_sound = sum(m_dev_no_sound)/(len(no_sound)-1)
+	z_var_no_sound = sum(z_dev_no_sound)/(len(no_sound)-1)
+
+
+
+	probability_e_sound = (1/numpy.sqrt(2*math.pi*e_var_sound)) * math.exp( (-(float(test_set[6][0])-e_average_sound)**2) / (2*(e_var_sound)**2) )
+	probability_m_sound = (1/numpy.sqrt(2*math.pi*m_var_sound)) * math.exp( (-(float(test_set[6][1])-m_average_sound)**2) / (2*(m_var_sound)**2) )
+	probability_z_sound = (1/numpy.sqrt(2*math.pi*z_var_sound)) * math.exp( (-(float(test_set[6][2])-z_average_sound)**2) / (2*(z_var_sound)**2) )
+
+	probability_e_nosound = (1/numpy.sqrt(2*math.pi*e_var_no_sound)) * math.exp( (-(float(test_set[6][0])-e_average_no_sound)**2) / (2*(e_var_no_sound)**2) )
+	probability_m_nosound = (1/numpy.sqrt(2*math.pi*m_var_no_sound)) * math.exp( (-(float(test_set[6][1])-m_average_no_sound)**2) / (2*(m_var_no_sound)**2) )
+	probability_z_nosound = (1/numpy.sqrt(2*math.pi*z_var_no_sound)) * math.exp( (-(float(test_set[6][2])-z_average_no_sound)**2) / (2*(z_var_no_sound)**2) )
+
+	prob_sound   = 0.5* probability_e_sound*probability_m_sound*probability_z_sound
+	prob_nosound = 0.5* probability_e_nosound*probability_m_nosound*probability_z_nosound
+
+	posterior_sound = prob_sound/(prob_sound+prob_nosound)
+	posterior_no_sound = prob_nosound/(prob_sound+prob_nosound)
+	print test_set[6]
+	print posterior_sound
+	print posterior_no_sound
 def main():
-	feature_value()
+
+	energy,magnitude,zcr,types = read()
+	kfold(energy,magnitude,zcr,types,10)
+
+	# feature_value()
 	# convolve("laboratory.txt",10)
 	# sampleRate("laboratory.txt")
 	# delayOp("laboratory.txt",10)
